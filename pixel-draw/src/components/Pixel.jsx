cec5fd1404d7725bf1edbad7c7a74c82e35baab8
Yanny Patel <47578696+Yanny24211@users.noreply.github.com>
2025-04-13 02:26:12 -0400

    added draw on drag functionality

--- pixel-draw/src/components/Pixel.jsx
+++ pixel-draw/src/components/Pixel.jsx
@@ -3,9 +3,25 @@ import './components.css'
 
 function Pixel(props){
     const { color } = props
+    const [isMouseDrag, setIsMouseDrag] = useState(false)
     const [pixelColor, setPixelColor] = useState('#fff');
     const [oldColor, setOldColor] = useState(pixelColor)
     const [canChangeColor, setCanChangeColor] = useState(true);
+
+    useState(()=>{
+        const handleMouseDown = () => {
+            console.log("mouse down bruh")
+            setIsMouseDrag(true);
+        }
+        const handleMouseUp = () => setIsMouseDrag(false);
+
+        window.addEventListener('mousedown', handleMouseDown);
+        window.addEventListener('mouseup', handleMouseUp);
+        return () => {
+            window.removeEventListener('mousedown', handleMouseDown);
+            window.removeEventListener('mouseup', handleMouseUp);
+        };
+    }, [])
 
     function changeColor(){
         setPixelColor(color);
@@ -25,7 +41,7 @@ function Pixel(props){
     }
 
     return( 
-        <div className="pixel" onClick={changeColor} onMouseEnter={showNewColor} onMouseLeave={handleReset} style={{ backgroundColor: pixelColor}}></div>
+        <div className="pixel" onMouseDown={changeColor} onMouseEnter={isMouseDrag ? changeColor :showNewColor } onMouseLeave={handleReset} style={{ backgroundColor: pixelColor}}></div>
     )
 }
 
